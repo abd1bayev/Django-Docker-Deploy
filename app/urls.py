f497ec3b42e679490d2d1bda63d97c92672e8cb5
student <jasurabdibayev0@gmail.com>
2022-11-29 19:18:56 +0500

    getbootstrap

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, add_expence
 
 
 urlpatterns = [
     path('', index),
+    path('add-expence/', add_expence),
+    
 ]
