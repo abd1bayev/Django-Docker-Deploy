84c67a75320cb5dffe798232d78e684058ed2009
student <jasurabdibayev0@gmail.com>
2022-11-30 11:54:39 +0500

    docker deploy

--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, add_expence
-
-
-urlpatterns = [
-    path('', index),
-    path('add-expence/', add_expence),
-    
-]
--- app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'expenses/index.html')
-
-def add_expence(request):
-    return render(request, 'expenses/add_expence.html')
